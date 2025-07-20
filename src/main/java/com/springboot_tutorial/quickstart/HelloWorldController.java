9b42b325463d32f6cac499db50d4d8d2928977d0
db <amjadrifaii@gmail.com>
2025-07-20 22:37:33 +0300

    create new folder

---
+++ src/main/java/com/springboot_tutorial/quickstart/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.springboot_tutorial.quickstart;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(path="/hello")
+    public String greet()
+    {
+        return "hello world";
+    }
+}
